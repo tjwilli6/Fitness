6c986bfd501ff3023ea35bcb52122d29c0db5607
Tyler Williamson <tjwilli@udel.edu>
2017-08-12 09:13:37 -0400

    Added fitnessdata class file

---
+++ fitnessdata.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 12 09:11:57 2017
+
+@author: tjwilliamson
+
+A class to interface with myfitnesspal and strava
+to track my fitness data
+"""
+
