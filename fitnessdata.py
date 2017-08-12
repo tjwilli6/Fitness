b22cd0a01ec38b1a77b37fa16f3fb77d602084b4
Tyler Williamson <tjwilli@udel.edu>
2017-08-12 11:10:50 -0400

    Updated progress on class

--- fitnessdata.py
+++ fitnessdata.py
@@ -12,21 +12,45 @@ to track my fitness data
 import myfitnesspal as mfp
 import stravalib as strava
 import datetime
+import os
 import numpy as np
 
+CREDENTIALS = 'credentials.txt'
 class FitnessData(object):
     
-    def __init__(self,start_date = None, stop_date = None):
-    
+    def __init__(self,start_date = None, stop_date = None, date_fmt = '%Y-%M-%d'):
+        self.date_fmt = date_fmt
+        self.start_date = self._set_date_(start_date)
+        self.stop_date = self._set_date_(stop_date)
+        self.credentials = {"MFP_USER":None,"STRAVA_TOKEN":None}
     
     def _set_date_(self,date):
         """Make sure date is a valid object"""
-        if type(date) in (datetime.date,datetime.datetime):
+        #If it is already a datetime object, or None
+        if type(date) in (datetime.date,datetime.datetime,type(None)):
             return date
         
+        #If it's a string, turn it into a datetime
         elif type(date) == str:
+            try:
+                date_obj = datetime.datetime.strptime(date,self.date_fmt)
+            except ValueError as e:
+                print e.args[0]
+                date_obj = None
+            return date_obj
     
-    def _read_date_string(datestr):
-        """Guess date format and read string into datetime"""
+    def _read_creds(self):
+        """Read login credentials"""
+        if not os.path.isfile(CREDENTIALS):
+            return {'MFPUSER':None,'STRAVATOKEN':None}
+        else:
+            with open(CREDENTIALS) as f:
+                for line in f:
+                    split = [l.strip() for l in line.split(':')]
+                    if len(split) == 2:
+                        key,attribute = split
+                        if self.credentials.has_key(key):
+                            self.credentials[key] = attribute
         
-
+                
+        
