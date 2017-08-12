4ad37663fe64619bdace5875dc44e63e592d666d
Tyler Williamson <tjwilli@udel.edu>
2017-08-12 17:19:15 -0400

    Adding DB update code

--- fitnessdata.py
+++ fitnessdata.py
@@ -15,21 +15,30 @@ import datetime
 import os
 import numpy as np
 
+#Define file names
 CREDENTIALS = 'credentials.txt'
+DB_WGT = 'db/mfpwt.dat'
+DB_CAL = 'db/mfpcl.dat'
+DB_RUN = 'db/st_rn.dat'
+
 class FitnessData(object):
     
     def __init__(self,start_date = None, stop_date = None, date_fmt = '%Y-%m-%d'):
+        #Read inputs and define variables
         self.date_fmt = date_fmt
         self._start_date = self._set_date_(start_date)
         self._stop_date = self._set_date_(stop_date)
-        self.credentials = {"MFP_USER":None,"STRAVA_TOKEN":None}
+        self._credentials = {"MFP_USER":None,"STRAVA_TOKEN":None}
+        
+        #Do we need to update the database?
+        today = datetime.datetime.today()
+        last_update = self.get_last_entry()
     
     def _set_date_(self,date):
         """Make sure date is a valid object"""
         #If it is already a datetime object, or None
         if type(date) in (datetime.date,datetime.datetime,type(None)):
             return date
-        
         #If it's a string, turn it into a datetime
         elif type(date) == str:
             try:
@@ -38,6 +47,9 @@ class FitnessData(object):
                 print e.args[0]
                 date_obj = None
             return date_obj
+        else:
+            print "Cannot make type %s into date object."%type(date)
+            return None
     
     def _read_creds(self):
         """Read login credentials"""
@@ -51,6 +63,19 @@ class FitnessData(object):
                         key,attribute = split
                         if self.credentials.has_key(key):
                             self.credentials[key] = attribute
+    def get_last_entry(self):
+        """Retrieve the date of the most recent entry"""
+        if not os.path.isfile(DB_CAL):
+            return None
+        else:
+            lastline = ""
+            with open(DB_CAL) as calfile:
+                for line in calfile:
+                    lastline = line
+            datestr = lastline.split(',')[0]
+            date = self._set_date_(datestr)
+            return date
+        
     @property
     def start_date(self):
         return self._start_date
